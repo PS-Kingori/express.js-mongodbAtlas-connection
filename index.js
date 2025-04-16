d237cd5a82748be1abe48dd5f37f1de3fa928c0e
PS-Kingori <peterskingori@gmail.com>
2025-04-16 13:04:49 +0300

    mongodb-atlas

--- index.js
+++ index.js
@@ -9,13 +9,41 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-mongoose.connect(process.env.MONGODB_URI,)
+mongoose.connect(process.env.MONGODB_URI2,)
     .then(() => {
         console.log('MongoDB connected successfully');
     })
     .catch(err => {
         console.error('MongoDB connection error:', err);
     });
+
+const movieSchema = new mongoose.Schema({}, { collection: 'movies' });
+const Movie = mongoose.model('Movie', movieSchema);
+
+
+app.get('/movies', async (req, res) => {
+    try {
+        const movies = await Movie.find();
+        res.json(movies);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get('/movies/:id', async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        if (!movie) {
+            return res.status(404).json({ message: 'Movie not found' });
+        }
+        res.json(movie);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
 
 app.listen(PORT, ()=>{
     console.log(`app is running`)
